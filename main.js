6ee286e8b1d929f5f028c3c6f4e813393dfd8e9e
Pierre Le <45873162+BokChoyBoys@users.noreply.github.com>
2023-12-29 00:39:37 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-//import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 //import * as CANNON from "cannon";
 
 function applyRotationToVector(offset, obj) {
